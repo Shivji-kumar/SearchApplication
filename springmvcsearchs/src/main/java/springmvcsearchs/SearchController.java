e943b4b1b4ee8a78cfd4c06112daff0cf55480bc
Shivji Kumar <95332763+Shivji-kumar@users.noreply.github.com>
2023-09-29 23:10:11 +0530

    Add files via upload

---
+++ springmvcsearchs/src/main/java/springmvcsearchs/SearchController.java
@@ -0,0 +1,33 @@
+package springmvcsearchs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+
+	@RequestMapping("/home")
+	public String home()
+	{
+		System.out.println("Going to home view");
+		return "home";
+	}
+	
+	@RequestMapping("/search")
+	public RedirectView search(@RequestParam("querybox") String query) {
+		
+		if(query==null)
+		{
+			
+		}
+		String url="https://www.google.com/search?q=" + query;
+		
+		RedirectView redirectView=new RedirectView();
+						
+		redirectView.setUrl(url);
+			
+		return redirectView;
+	}
+}
